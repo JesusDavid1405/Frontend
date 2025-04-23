000de80709802b59a893a00b377ba7bad1e9074b
jesus david fierro rivera <jesusdavidfierrorivera817@gmail.com>
2025-04-23 01:06:59 -0500

    login

---
+++ main.js
@@ -0,0 +1,10 @@
+let btnLogin = document.getElementById('BtnLogin');
+let btnRegistred = document.getElementById('BtnRegistred');
+
+btnLogin.addEventListener('click',async function (){
+    window.location.href='src/pages/Login.html'
+});
+
+btnRegistred.addEventListener('click',async function (){
+    window.location.href='src/pages/Registre.html'
+});
